34db93f336ae9bbf12018b309773033881c75f61
dynemre <102437310+dynemre@users.noreply.github.com>
2022-03-27 00:27:52 +0300

    Add files via upload

---
+++ util/ayarlar.js
@@ -0,0 +1,6 @@
+{
+  "prefix": "!",
+  "sahip": "447869093475778581",
+  "token": "OTM5OTAwNzUwMDA2NDY4NzA4.Yf_kyQ.E6_Llohm8kLe882yztRQZCXm0bc"
+  "YOUTUBE_API_KEY" : "AIzaSyAyKCCRpaEsumncmm2C1onUQBaSV_Sba3I"
+}  
